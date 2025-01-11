63b64a5e9682725b6466a18ca5f7389a34c1847b
Neema <patelneema93@gmail.com>
2025-01-10 22:09:16 -0500

    set limitation using private route

--- task/src/router/PrivateRoute.jsx
+++ task/src/router/PrivateRoute.jsx
@@ -1,4 +1,28 @@
-const PrivateRoute = () => {
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+// import { getExpenses } from "../actions/expenses";
+// import Header from "../components/Header";
+const PrivateRoute = (props) => {
+  const { isAuthenticated } = props;
+  useEffect(() => {
+    if (isAuthenticated) {
+    //   props.dispatch(getExpenses()); // Dispatch the action to fetch expenses
+    } else {
+      console.warn("User is not authenticated. Cannot fetch expenses.");
+    }
+  }, [isAuthenticated, props]);
 
-}
-export default PrivateRoute;
+  return props.isAuthenticated ? (
+    <>
+      {/* <Header /> Render the header here */}
+      <Outlet /> {/* Render the child routes */}
+    </>
+  ) : (
+    <Navigate to="/" />
+  );
+};
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.authentication.isAuthenticated,
+});
+export default connect(mapStateToProps)(PrivateRoute);
