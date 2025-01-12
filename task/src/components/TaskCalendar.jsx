ec6557ea8efd50fc1f5abde28a566ff2e91507f3
Neema <patelneema93@gmail.com>
2025-01-12 15:04:14 -0500

    create css for highlight calender date

--- task/src/components/TaskCalendar.jsx
+++ task/src/components/TaskCalendar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { connect } from "react-redux";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import "./../style/TaskCalendar.css"
 const TaskCalendar = (props) => {
     const [selectedDate, setSelectedDate] = useState(new Date());
     const tasksForDate = props.tasks.filter(
@@ -11,15 +12,14 @@ const TaskCalendar = (props) => {
         setSelectedDate(date);
       };
       const getTileClassName = ({ date }) => {
-        // Check if there are tasks on this date
         const hasTasks = props.tasks.some(
           (task) => task.deadline.toDateString() === date.toDateString()
         );
-        return hasTasks ? "highlight" : ""; // Apply highlight class if there are tasks
+        return hasTasks ? "highlight" : "";
       };
     return(
         <>
-          <div style={{ marginBottom: "20px", margin: '0 auto' }}>
+          <div style={{ marginBottom: "20px" }}>
         <Calendar value={selectedDate} onChange={handleDateChange} tileClassName={getTileClassName} />
       </div>
       <h3>Tasks for {selectedDate.toDateString()}</h3>
